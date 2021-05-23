996fa36ad229de0e090e6a1b474d80a45b061089
Luis Chiang <lahoising78@gmail.com>
2021-05-22 23:11:12 -0400

    definitions compile to nothing when in release mode

--- src/include/mr_logger.h
+++ src/include/mr_logger.h
@@ -1,17 +1,19 @@
 #ifndef _MR_LOGGER_H_
 #define _MR_LOGGER_H_
 
-#include <cstdlib>
-
 #ifdef _cplusplus
+#include <cstdlib>
 extern "C"
 {
+#else
+#include <stdarg.h>
 #endif
 
 #define MR_LOG_BIT_UNKNOWN    0
 #define MR_LOG_BIT_INFO       1
 #define MR_LOG_BIT_WARN       2
 #define MR_LOG_BIT_ERR        4
+#define MR_LOG_BIT_ALL        MR_LOG_BIT_INFO | MR_LOG_BIT_WARN | MR_LOG_BIT_ERR
 
 #define MR_LOG_ACTION_UNKNOWN 0
 #define MR_LOG_ACTION_ON      1
@@ -20,14 +22,18 @@ extern "C"
 
 typedef char MobRendLogBitMask;
 
+#ifndef NDEBUG
 #define mrlog(...)  _mr_log(__FILE__,__LINE__,"","",MR_LOG_BIT_INFO,__VA_ARGS__)
 #define mrerr(...)  _mr_log(__FILE__, __LINE__, "\033[1;31m", "\033[0m",MR_LOG_BIT_ERR,__VA_ARGS__)
 #define mrwarn(...) _mr_log(__FILE__, __LINE__, "\033[1;33m", "\033[0m",MR_LOG_BIT_WARN,__VA_ARGS__)
+#else
+#define mrlog(...)
+#define mrerr(...)
+#define mrwarn(...)
+#endif
 
 void _mr_log(const char *f, int l, const char *colorStart, const char *colorEnd,char priority,const char *msg,...);
-void mr_log_filter_output(MobRendLogBitMask bitMask, char logAction);
-
-void baka_log_init();
+void mr_log_filter_output(MobRendLogBitMask mask, char logAction);
 
 #define BAKA_ASSERT(expr)\
 if((expr))\
--- src/mr_logger.c
+++ src/mr_logger.c
@@ -1,23 +1,66 @@
+#include <stdio.h>
 #include "mr_logger.h"
 
 #ifdef _cplusplus
+#include <cstring>
 extern "C"
 {
+#else
+#include <string.h>
 #endif
 
-void baka_log_init()
+#if defined(_WIN32)
+#define MR_FILE_SEPARATOR '\\'
+#endif
+
+typedef struct
 {
-    
-}
+    MobRendLogBitMask msgMask;
+} MobRendLogger;
+
+static MobRendLogger g_mr_logger = { MR_LOG_BIT_ALL };
+
+const char *logger_get_filename(const char *filename);
 
 void _mr_log(const char *f, int l, const char *colorStart, const char *colorEnd,char priority,const char *msg,...)
 {
+    va_list va;
 
+    if( !(g_mr_logger.msgMask & priority) ) return;
+    
+    va_start(va, msg);
+    printf("%s%s(%d):%s ", colorStart, logger_get_filename(f), l, colorEnd);
+    vprintf(msg, va);
+    printf("\n");
+    va_end(va);
 }
 
-void mr_log_filter_output(MobRendLogBitMask bitMask, char logAction)
+const char *logger_get_filename(const char *filename)
 {
+    const char *sepPos = strrchr(filename, MR_FILE_SEPARATOR);
 
+    return sepPos? sepPos + 1 : filename;
+}
+
+void mr_log_filter_output(MobRendLogBitMask mask, char logAction)
+{
+    switch (logAction)
+    {
+    case MR_LOG_ACTION_ON:
+        g_mr_logger.msgMask |= mask;
+        break;
+
+    case MR_LOG_ACTION_OFF:
+        g_mr_logger.msgMask &= ~mask;
+        break;
+    
+    case MR_LOG_ACTION_SET:
+        g_mr_logger.msgMask = mask;
+        break;
+
+    default:
+        break;
+    }
 }
 
 
